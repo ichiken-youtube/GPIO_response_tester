24a97a287dedc05683fd0c01c970adca9466914c
ICHIKEN <inquiry@ichiken-engineering.com>
2024-02-19 18:23:55 +0900

    RP2040レジスタ書き換え方式

---
+++ RP2040_echo/RP2040_echo.ino
@@ -0,0 +1,23 @@
+#define GPIO_OUT_SET (sio_hw->gpio_set)
+#define GPIO_OUT_CLR (sio_hw->gpio_clr)
+#define GPIO_IN_BTN (sio_hw->gpio_in)
+volatile byte state = LOW;
+
+void setup() {
+  pinMode(16, OUTPUT);
+  pinMode(17, INPUT_PULLUP);
+  GPIO_OUT_CLR |= 1ul << 16;
+  while(1){
+    if((GPIO_IN_BTN >> 17)%2){
+      GPIO_OUT_SET |= 1ul << 16;
+    }else{
+      GPIO_OUT_CLR |= 1ul << 16;
+    }
+  }
+
+}
+
+void loop() {
+  delay(3000);
+  GPIO_OUT_SET |= 1ul << 16;
+}
