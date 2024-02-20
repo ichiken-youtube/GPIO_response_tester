b7b14e70c586d42e9176ea08400111ffd32b279b
ICHIKEN <inquiry@ichiken-engineering.com>
2024-02-20 09:54:47 +0900

    ハードウェア割込み版

---
+++ RP2040_echo_int/RP2040_echo_int.ino
@@ -0,0 +1,21 @@
+#define GPIO_OUT_SET (sio_hw->gpio_set)
+#define GPIO_OUT_CLR (sio_hw->gpio_clr)
+#define GPIO_IN_BTN (sio_hw->gpio_in)
+volatile byte state = LOW;      // volatileをつけて宣言
+
+void setup() {
+  pinMode(16, OUTPUT);
+  pinMode(17, INPUT_PULLUP);
+  GPIO_OUT_CLR |= 1ul << 16;
+  attachInterrupt(digitalPinToInterrupt(17),set,FALLING);
+
+}
+
+void loop() {
+  delay(3000);
+  GPIO_OUT_CLR |= 1ul << 16;
+}
+
+void set(){
+  GPIO_OUT_SET |= 1ul << 16;
+}
