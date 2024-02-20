727c1c8ae8543457653ae743bef922d548485ce3
ICHIKEN <inquiry@ichiken-engineering.com>
2024-02-20 09:57:35 +0900

    引数の処理を簡略化

--- RasPi_echo.c
+++ RasPi_echo.c
@@ -28,9 +28,7 @@ int main(int argc, char *argv[]) {
     if(argc == 1){
         chip = gpiod_chip_open("/dev/gpiochip0");
     }else{
-        if(strcmp(argv[1],"pi4")==0){
-            chip = gpiod_chip_open("/dev/gpiochip0");
-        }else if(strcmp(argv[1],"pi5")==0){
+        if(strcmp(argv[1],"pi5")==0){
             chip = gpiod_chip_open("/dev/gpiochip4");
             printf("This program is running on RaspberryPi5!!");
         }else{
